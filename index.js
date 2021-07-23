30f5173ca080001ed9a54b609586e1b484fe9f62
stevenjhomem <sjhomem@outlook.com>
2021-07-23 12:39:39 -0500

    Finished final tasks with the cuboid and inheritance, I also did the stretch task

--- index.js
+++ index.js
@@ -224,19 +224,52 @@ console.log(cuboid.surfaceArea()); // 130
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo{
+  constructor(properties){
+    this.width = properties.width,
+    this.length = properties.length,
+    this.height = properties.height
+  }
+
+  volume(){
+    return this.width*this.length*this.height;
+  }
+
+  surfaceArea(){
+    return 2*(this.width*this.length+this.height*this.length+this.width*this.height);
+  }
 
 }
 
+const cuboidTwo = new CuboidMakerTwo({
+  width: 5,
+  length: 4,
+  height: 5
+})
+
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-// console.log(cuboidTwo.volume()); // 100
-// console.log(cuboidTwo.surfaceArea()); // 130
+console.log(cuboidTwo.volume()); // 100
+console.log(cuboidTwo.surfaceArea()); // 130
 
 
 
 
 
 // 🦄 💪 Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area. 🦄 💪
+
+class CubeMake extends CuboidMaker {
+
+  constructor(properties){
+    super(properties);
+  }
+
+  volume(){
+    return Math.pow(this.length,3);
+  }
+  surfaceArea(){
+    return 6*(this.length*this.width);
+  }
+}
   
 
 
